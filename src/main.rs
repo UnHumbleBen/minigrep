ae419af0cfee2b6cb9f8070674904133e90ce768
Benjamin Lee <bnllee@ucdavis.edu>
2018-12-28 20:12:59 -0800

    grouped configurations together

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,30 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // saving the argument values
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     // reading the file
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+// grouping configuration values
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    // constructor for Config
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
