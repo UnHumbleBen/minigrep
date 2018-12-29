2eae887dfa212274ad43bc6d01594a0e0cb8a44b
Benjamin Lee <bnllee@ucdavis.edu>
2018-12-28 20:27:45 -0800

    Fixed error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // reading the argument values
     let args: Vec<String> = env::args().collect();
 
     // saving the argument values
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // handling error
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -26,10 +31,15 @@ struct Config {
 
 impl Config {
     // constructor for Config
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        // error message
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
