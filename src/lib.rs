8abcc549bf142c187b0d001fb4c8d8517389f4d0
Benjamin Lee <bnllee@ucdavis.edu>
2018-12-28 21:07:12 -0800

    begin testing

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,28 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // reading the file
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
 }
